677f541ffc71c919dc26f08a83a93497195bc854
GuilhermeWillahelm <guilhermewlm@outlook.com>
2022-02-03 11:37:38 -0300

    Created DataContex, StringConnection and Migration

--- EcommerceApp/Server/Program.cs
+++ EcommerceApp/Server/Program.cs
@@ -1,15 +1,22 @@
 global using EcommerceApp.Server;
 global using EcommerceApp.Shared;
+global using Microsoft.EntityFrameworkCore;
+using EcommerceApp.Server.Data;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options => { options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); });
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -23,6 +30,7 @@ else
     app.UseHsts();
 }
 
+app.UseSwagger();
 app.UseHttpsRedirection();
 
 app.UseBlazorFrameworkFiles();
