cb7d560efbbdd317d670550a6484b026e13167ff
GuilhermeWillahelm <guilhermewlm@outlook.com>
2022-02-04 13:40:17 -0300

    Added ProductDetails and implements generics and dependency injection

---
+++ EcommerceApp/Server/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        //private readonly DataContext _context;
+        private readonly IProductService _productService;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public ProductController(IProductService productService)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+            _productService = productService;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProducts()
+        {
+            /*
+            var products = await _context.Products.ToListAsync();
+            var response = new ServiceResponse<List<Product>>()
+            {
+               Data = products
+            };
+            */
+            var result = await _productService.GetProductsAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{productId}")]
+        public async Task<ActionResult<ServiceResponse<Product>>> GetProduct(int productId)
+        {
+            var result = await _productService.GetProductAsync(productId);
+            return Ok(result);
+        } 
+    }
+}
