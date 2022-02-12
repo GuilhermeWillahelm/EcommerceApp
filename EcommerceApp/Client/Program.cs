ee367e360dee361db56805ae3f34676220c53e54
GuilhermeWillahelm <guilhermewlm@outlook.com>
2022-02-12 10:44:54 -0300

    Adding AuthService and Authentication in Client

--- EcommerceApp/Client/Program.cs
+++ EcommerceApp/Client/Program.cs
@@ -2,16 +2,23 @@ global using EcommerceApp.Shared;
 global using System.Net.Http.Json;
 global using EcommerceApp.Client.Services.ProductService;
 global using EcommerceApp.Client.Services.CategoryService;
+global using Microsoft.AspNetCore.Components.Authorization;
 using EcommerceApp.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+
 
 await builder.Build().RunAsync();
