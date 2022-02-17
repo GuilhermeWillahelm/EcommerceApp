07ea72e63a6dbe47607db26b6a0c8438d179e85d
GuilhermeWillahelm <guilhermewlm@outlook.com>
2022-02-17 14:54:48 -0300

    Added Services Order and Cart

---
+++ EcommerceApp/Client/Services/OrderService/IOrderService.cs
@@ -0,0 +1,9 @@
+﻿namespace EcommerceApp.Client.Services.OrderService
+{
+    public interface IOrderService
+    {
+        Task<string> PlacerOder();
+        Task<List<OrderOverviewResponse>> GetOrders();
+        Task<OrderDetailsResponse> GetOrderDetails(int orderId);
+    }
+}
