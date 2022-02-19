03c11bf377984045de6b99c359b1112c1b43afc2
GuilhermeWillahelm <guilhermewlm@outlook.com>
2022-02-19 09:14:55 -0300

    Added CategoryAdmin and modified Category Services

--- EcommerceApp/Server/Services/CategoryService/CategoryService.cs
+++ EcommerceApp/Server/Services/CategoryService/CategoryService.cs
@@ -8,32 +8,80 @@
         {
             _context = context;
         }
-        public async Task<ServiceResponse<List<Category>>> GetCategories()
-        {
-            var response = new ServiceResponse<List<Category>>() {
-                Data = await _context.Categories.ToListAsync()
-            };
 
-            return response;
+        public async Task<ServiceResponse<List<Category>>> AddCategory(Category category)
+        {
+            category.Editing = category.IsNew = false;
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+            return await GetAdminCategories();
         }
 
-        public async Task<ServiceResponse<Category>> GetCategory(int categoryId)
+        public async Task<ServiceResponse<List<Category>>> DeleteCategory(int id)
         {
-            var response = new ServiceResponse<Category>();
-            var category = await _context.Categories.FindAsync(categoryId);
-
+            Category category = await GetCategoryById(id);
             if (category == null)
             {
-                response.Success = false;
-                response.Message = "Sorry, but this category does not exits.";
-            }
-            else
-            {
-                response.Success = true;
-                response.Data = category;
+                return new ServiceResponse<List<Category>>
+                {
+                    Success = false,
+                    Message = "Category not found."
+                };
             }
 
-            return response;
+            category.Deleted = true;
+            await _context.SaveChangesAsync();
+
+            return await GetAdminCategories();
+        }
+
+        private async Task<Category> GetCategoryById(int id)
+        {
+            return await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<ServiceResponse<List<Category>>> GetAdminCategories()
+        {
+            var categories = await _context.Categories
+                .Where(c => !c.Deleted)
+                .ToListAsync();
+            return new ServiceResponse<List<Category>>
+            {
+                Data = categories
+            };
+        }
+
+        public async Task<ServiceResponse<List<Category>>> GetCategories()
+        {
+            var categories = await _context.Categories
+                .Where(c => !c.Deleted && c.Visible)
+                .ToListAsync();
+            return new ServiceResponse<List<Category>>
+            {
+                Data = categories
+            };
+        }
+
+        public async Task<ServiceResponse<List<Category>>> UpdateCategory(Category category)
+        {
+            var dbCategory = await GetCategoryById(category.Id);
+            if (dbCategory == null)
+            {
+                return new ServiceResponse<List<Category>>
+                {
+                    Success = false,
+                    Message = "Category not found."
+                };
+            }
+
+            dbCategory.Name = category.Name;
+            dbCategory.Url = category.Url;
+            dbCategory.Visible = category.Visible;
+
+            await _context.SaveChangesAsync();
+
+            return await GetAdminCategories();
+
         }
     }
 }
